2d24f072cd9d7dc575a70ecdc87f317560e9ff8f
heesu <oung1002@snu.ac.kr>
2020-11-24 01:24:34 +0900

    Add login layout
    Add login layout

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import LoginForm from "../Components/LoginFom";
+import { Box, Image, Stack } from "@chakra-ui/react";
 
 const Login = () => {
-  return <div> login </div>;
+  return (
+    <Stack align="center" justify="center" pt="60">
+      <Link to="/">
+        <img
+          src="https://everytime.kr/images/index.login.logo.png"
+          alt="everytime logo"
+        ></img>
+      </Link>
+      <LoginForm />
+    </Stack>
+  );
 };
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
