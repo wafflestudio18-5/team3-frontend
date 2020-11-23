37b8eb3910297636916b6c126384dafaebab7fb3
heesu <oung1002@snu.ac.kr>
2020-11-23 23:53:10 +0900

    add basic route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-
-import Main from "./Pages/Main";
+import routes from "./Routes";
 
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/" component={Main} />
+        <Route exact path="/" component={routes.main.component} />
+        <Route path={routes.login.path} component={routes.login.component} />
+        <Route
+          path={routes.register.path}
+          component={routes.register.component}
+        />
         <Redirect path="*" to="/" />
       </Switch>
     </>
---
+++ src/Routes.js
@@ -0,0 +1,10 @@
+import Main from "./Pages/Main";
+import Login from "./Pages/Login";
+// import Posts from "./pages/Posts";
+import Register from "./Pages/Register";
+
+export default {
+  main: { path: "/", component: Main },
+  login: { path: "/login", component: Login },
+  register: { path: "/register", component: Register },
+};
