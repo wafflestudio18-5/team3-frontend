e5ab629f55e7b5437799b1d254428a437d2bc8ed
heesu <oung1002@snu.ac.kr>
2020-11-24 02:10:44 +0900

    Add Footer component

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,19 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LoginForm from "../Components/LoginFom";
+
 import { Box, Image, Stack } from "@chakra-ui/react";
+
+import LoginForm from "../Components/LoginFom";
+import Footer from "../Components/Footer/Footer.js";
 
 const Login = () => {
   return (
-    <Stack align="center" justify="center" pt="60">
-      <Link to="/">
-        <img
-          src="https://everytime.kr/images/index.login.logo.png"
-          alt="everytime logo"
-        ></img>
-      </Link>
-      <LoginForm />
-    </Stack>
+    <>
+      <Stack align="center" justify="center" pt="60">
+        <Link to="/">
+          <img
+            src="https://everytime.kr/images/index.login.logo.png"
+            alt="everytime logo"
+          ></img>
+        </Link>
+        <LoginForm />
+      </Stack>
+      <Footer />
+    </>
   );
 };
 
