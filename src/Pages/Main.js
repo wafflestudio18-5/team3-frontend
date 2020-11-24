b5196a7165e333881dc756933ff98f9021bc0f27
heesu <oung1002@snu.ac.kr>
2020-11-25 02:33:52 +0900

    temporay

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import NavBar from "../Components/NavBar/NavBar";
 const Main = () => {
   return (
     <div>
+      <NavBar />
       <h1>Welcome!</h1>
     </div>
   );
