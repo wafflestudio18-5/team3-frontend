281481e83dbdc477edbb8c344fea6baba280ea91
heesu <oung1002@snu.ac.kr>
2020-11-27 22:38:39 +0900

    Add university list in aside  ,add university search logic

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,14 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Stack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar.js";
-
+import SearchList from "./SearchList";
 import "./Aside.css";
 
+const campusList = [
+  {
+    name: "서울대학교",
+    link: "/login",
+  },
+  {
+    name: "연세대학교",
+    link: "/snu",
+  },
+  {
+    name: "고려대학교",
+    link: "/snu",
+  },
+  {
+    name: "카이스트",
+    link: "/snu",
+  },
+  {
+    name: "건국대학교",
+    link: "/snu",
+  },
+  {
+    name: "서울대학교",
+    link: "/snu",
+  },
+  {
+    name: "연세대학교",
+    link: "/snu",
+  },
+  {
+    name: "고려대학교",
+    link: "/snu",
+  },
+  {
+    name: "카이스트",
+    link: "/snu",
+  },
+  {
+    name: "건국대학교",
+    link: "/snu",
+  },
+  {
+    name: "서울대학교",
+    link: "/snu",
+  },
+  {
+    name: "연세대학교",
+    link: "/snu",
+  },
+  {
+    name: "고려대학교",
+    link: "/snu",
+  },
+  {
+    name: "카이스트",
+    link: "/snu",
+  },
+  {
+    name: "건국대학교",
+    link: "/snu",
+  },
+];
+
 const Aside = () => {
+  const [uniList, setUniList] = useState(campusList);
+  const onUniChange = (list) => {
+    setUniList(list);
+  };
   return (
     <aside className="aside">
-      <div className="login-section">
+      <div className="login-section bottom-border">
         <Link to="/" className="logo-img">
           <img
             src="https://everytime.kr/images/about/logo.png"
@@ -29,9 +96,11 @@ const Aside = () => {
         <SearchBar
           label="우리 학교 커뮤니티 둘러보기"
           placeHolder="찾으시는 캠퍼스를 검색하세요"
+          list={campusList}
+          onUniChange={onUniChange}
         ></SearchBar>
       </div>
-      <section className="campus-list"></section>
+      <SearchList list={uniList} />
     </aside>
   );
 };
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 import { Input } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
-const SearchBar = ({ label, placeHolder, onSubmit }) => {
+
+const SearchBar = ({ label, placeHolder, onUniChange, list }) => {
   const [input, setInput] = useState("");
   const onInputChange = (e) => {
     setInput(e.target.value);
+    onInputSubmit(e.target.value);
   };
 
-  const onInputSubmit = () => {
-    onSubmit(input);
+  const onInputSubmit = (name) => {
+    let uniList = list.filter((item) => {
+      const length = name.length - 1;
+      if (item.name.search(name.slice(0, length)) !== -1) {
+        return true;
+      }
+      return false;
+    });
+    onUniChange(uniList);
   };
 
   return (
-    <form onSubmit={onInputSubmit} className="searchForm">
+    <form className="searchForm">
       <label className="search-label">{label}</label>
       <Input
         size="md"
         variant="filled"
-        placeHolder={placeHolder}
+        placeholder={placeHolder}
         value={input}
         onChange={onInputChange}
         icon="SearchIcon"
