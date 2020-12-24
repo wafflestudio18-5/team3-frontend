a0b27a82ad805e90a7ede0025cb34115de91116c
heesu <oung1002@snu.ac.kr>
2020-12-24 19:18:10 +0900

    Add CountUp hook

---
+++ src/hooks/useScrollCount.js
@@ -0,0 +1,39 @@
+import { useRef, useEffect, useCallback } from "react";
+
+const useScrollCount = (end, start = 0, duration = 1000, delay = 0) => {
+  const element = useRef();
+  let observer = {};
+  const stepTime = Math.abs(Math.floor(duration / (end - start)));
+
+  const onScroll = useCallback(
+    ([entry]) => {
+      const { current } = element;
+      if (entry.isIntersecting) {
+        let currentNumber = start;
+        const counter = setInterval(() => {
+          currentNumber++;
+          current.innerHTML = currentNumber;
+          if (currentNumber === end) {
+            clearInterval(counter);
+          }
+        }, stepTime);
+      }
+    },
+    [end, start, stepTime, element]
+  );
+
+  useEffect(() => {
+    if (element.current) {
+      observer = new IntersectionObserver(onScroll, { threshold: 0.8 });
+      observer.observe(element.current);
+    }
+
+    return () => observer && observer.disconnect();
+  }, [onScroll]);
+
+  return {
+    ref: element,
+  };
+};
+
+export default useScrollCount;
