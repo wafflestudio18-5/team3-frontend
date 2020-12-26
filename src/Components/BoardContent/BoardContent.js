e5bf8fce05a5abc95e1cbf670af04e9aaeac97c4
ZyeLK <hengunkaduk@naver.com>
2020-12-26 10:52:48 +0900

    submenu

--- src/Components/BoardContent/BoardContent.js
+++ src/Components/BoardContent/BoardContent.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 
 import { useListContext } from "../../Context/BoardList";
--- src/Components/Submenu/Submenu.js
+++ src/Components/Submenu/Submenu.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+import "./Submenu.css";
+
 function Submenu() {
-  return <div className="submenu"></div>;
+  return (
+    <header className="submenu">
+      <div className="submenu-content">
+        <ul className="submenu-column">
+          <li className="submenu-text">
+            <a href="/1">자유게시판</a>
+          </li>
+          <li className="submenu-text">
+            <a href="/2">비밀게시판</a>
+          </li>
+        </ul>
+        <ul className="submenu-column">
+          <li className="submenu-text">
+            <a href="/3">졸업생게시판</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 }
 
 export default Submenu;
