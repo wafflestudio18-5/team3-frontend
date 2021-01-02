b49e14c0a0f3066ae02421debaa1530be45a373e
heesu <oung1002@snu.ac.kr>
2021-01-02 15:50:50 +0900

    Add index.js for export easily

---
+++ src/Pages/index.js
@@ -0,0 +1,8 @@
+import Board from "./Board";
+import { UserInfo } from "./UserInfo";
+import Main from "./Main";
+import Login from "./Login";
+import Post from "./Post";
+import { Register } from "./Register";
+
+export { Board, UserInfo, Main, Login, Post, Register };
