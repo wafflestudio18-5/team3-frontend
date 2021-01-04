d0dda57f77c28de7476c96c3cb8f86932e6a1891
heesu <oung1002@snu.ac.kr>
2021-01-04 17:04:10 +0900

    change pages/index

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,5 +1,5 @@
 import Board from "./Board";
-import { UserInfo } from "./UserInfo";
+import UserInfo from "./UserInfo";
 import Main from "./Main";
 import Login from "./Login";
 import Post from "./Post";
