7eedcea21c4ec92d589205646f772076ae02244d
heesu <oung1002@snu.ac.kr>
2021-01-05 20:39:48 +0900

    Make Wrapper for reuse in my page

--- src/Components/Wrapper/MidWrapper.js
+++ src/Components/Wrapper/MidWrapper.js
@@ -1,7 +1,7 @@
 const MidWrapper = ({ children }) => {
   return (
     <section className="middle-flex-box">
-      <article className="light-border-wrapper pad-10">{children}</article>
+      <article className="light-border-wrapper  pad-10">{children}</article>
     </section>
   );
 };
