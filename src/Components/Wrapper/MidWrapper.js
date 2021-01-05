8479d04737adb65f95267be785005f41a0855eb2
heesu <oung1002@snu.ac.kr>
2021-01-05 18:12:31 +0900

    Add flex-wrapper

---
+++ src/Components/Wrapper/MidWrapper.js
@@ -0,0 +1,9 @@
+const MidWrapper = ({ children }) => {
+  return (
+    <section className="middle-flex-box">
+      <article className="light-border-wrapper pad-10">{children}</article>
+    </section>
+  );
+};
+
+export default MidWrapper;
