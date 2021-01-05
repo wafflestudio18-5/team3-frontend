5516cbabf55f5c46ad368d3d77bb6c9e3b5ed33e
heesu <oung1002@snu.ac.kr>
2021-01-05 20:46:13 +0900

    add auth routing

--- src/Pages/UserInfo/UserInfo.js
+++ src/Pages/UserInfo/UserInfo.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import { Email, My, Password, Withdrawal, Auth } from "./";
+import { Email, My, Password, Withdrawal, AuthRoute } from "./";
 const UserInfo = () => {
   return (
     <Switch>
       <Route exact path="/my" component={My}></Route>
-      <Route path="/my/auth" component={Auth}></Route>
+      <Route path="/my/auth" component={AuthRoute}></Route>
       <Route path="/my/email" component={Email}></Route>
       <Route path="/my/password" component={Password}></Route>
       <Route path="/my/withdrawal" component={Withdrawal}></Route>
