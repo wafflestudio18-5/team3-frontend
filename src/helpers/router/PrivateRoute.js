df00195f4b3c474120973c5064b6fea316ea012e
heesu <oung1002@snu.ac.kr>
2021-01-07 13:38:56 +0900

    change rendering function

--- src/helpers/router/PrivateRoute.js
+++ src/helpers/router/PrivateRoute.js
@@ -1,13 +1,12 @@
 import { Redirect, Route } from "react-router-dom";
-import { useCookies } from "react-cookie";
-const PrivateRoute = ({ component: Component, renderDecision, ...rest }) => {
-  const [cookie] = useCookies();
-  // renderDecision = renderDecision || cookie["waverytime"] !== undefined;
+import { useLoginContext } from "../../Context/LoginData";
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const { isLogined } = useLoginContext();
   return (
     <Route
       {...rest}
       render={(props) =>
-        renderDecision ? <Component {...props} /> : <Redirect to="/login" />
+        isLogined() ? <Component {...props} /> : <Redirect to="/login" />
       }
     />
   );
