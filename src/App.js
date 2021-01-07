1da50dbf232bd3b31c3489dcb0164a5746d3e07a
heesu <oung1002@snu.ac.kr>
2021-01-07 13:01:05 +0900

    change route to main route
    add logout function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
+import { MainRoute } from "./helpers/router/";
 import routes from "./Routes";
-import { CookiesProvider } from "react-cookie";
 import "./App.css";
 
 function App() {
   return (
-    <CookiesProvider>
-      <Switch>
-        <Route
-          exact
-          path={routes.main.path}
-          component={routes.main.component}
-        />
-        <Route path={routes.login.path} component={routes.login.component} />
-        <Route
-          path={routes.register.path}
-          component={routes.register.component}
-        />
-        <Route
-          path={routes.UserInfo.path}
-          component={routes.UserInfo.component}
-        />
-        <Route
-          exact
-          path={routes.board.path}
-          component={routes.board.component}
-        />
-        <Route path={routes.post.path} component={routes.post.component} />
-        <Redirect path="*" to={routes.main.path} />
-      </Switch>
-    </CookiesProvider>
+    <Switch>
+      <MainRoute
+        exact
+        path={routes.main.path}
+        component={routes.main.component}
+      />
+      <Route path={routes.login.path} component={routes.login.component} />
+      <Route
+        path={routes.register.path}
+        component={routes.register.component}
+      />
+      <Route
+        path={routes.UserInfo.path}
+        component={routes.UserInfo.component}
+      />
+      <Route
+        exact
+        path={routes.board.path}
+        component={routes.board.component}
+      />
+      <Route path={routes.post.path} component={routes.post.component} />
+      <Redirect path="*" to={routes.main.path} />
+    </Switch>
   );
 }
 
