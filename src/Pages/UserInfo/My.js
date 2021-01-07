3cc49dd8420704fabcb9658ba63bf0297dbcb238
heesu <oung1002@snu.ac.kr>
2021-01-07 14:52:27 +0900

    Add logout func to btn, change user info

--- src/Pages/UserInfo/My.js
+++ src/Pages/UserInfo/My.js
@@ -1,10 +1,13 @@
 import { InfoData, InfoWrapper } from "../../Components/UserInfo";
-import NavBar from "../../Components/NavBar/NavBar";
-import { Footer } from "../../Components/Footer";
+import { Footer, NavBar } from "../../Components/";
 import UserImg from "../../Images/user.png";
+import { useLoginContext } from "../../Context/LoginData";
+
 import "./InfoPage.css";
 
 const My = () => {
+  const { user, logout } = useLoginContext();
+  const { username, nickname, year, last_name, university } = user;
   const renderedContents = InfoData.map((section, index) => {
     return <InfoWrapper section={section} key={index}></InfoWrapper>;
   });
@@ -16,15 +19,19 @@ const My = () => {
         <div className="light-border-wrapper my-info">
           <div className="flex-nowrap-space">
             <h2 className="info-header-font">내 정보</h2>
-            <button onClick={() => {}}>로그아웃</button>
+            <button onClick={logout}>로그아웃</button>
           </div>
           <div>
             <img src={UserImg} alt="userImg" />
             <div className="gray-description-12">
               {/* 수정 필요 */}
-              <p>아이디</p>
-              <p>이름/닉네임</p>
-              <p>와플대 20학번</p>
+              <p>{username}</p>
+              <p>
+                {last_name}/{nickname}
+              </p>
+              <p>
+                {university} {year % 100}학번
+              </p>
             </div>
           </div>
         </div>
